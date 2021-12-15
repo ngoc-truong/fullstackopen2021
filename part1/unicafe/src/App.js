11340329d62fe3bd98074f32a1fc977ae9de5c3d
Ngoc Truong <ngoc.truong@posteo.de>
2021-12-15 21:54:43 +0100

    Finish task unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad, all }) => {
+  if (all === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine
+            text="average"
+            value={(good * 1 + neutral * 0 + bad * -1) / all}
+          />
+          <StatisticLine text="positive" value={good / all} />
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  );
+}
+
+export default App;
